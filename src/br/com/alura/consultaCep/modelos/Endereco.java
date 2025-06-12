ffc5b256bc87b9c6392d39925ebc68af46ec2d13
Marcus Vinicius <lucilenebarbosamoura@gmail.com>
2025-06-11 21:19:51 -0300

    Código refatorado!

--- src/br/com/alura/consultaCep/modelos/Endereco.java
+++ src/br/com/alura/consultaCep/modelos/Endereco.java
@@ -1,4 +1,42 @@
 package br.com.alura.consultaCep.modelos;
 
+import br.com.alura.consultaCep.excecoes.BuscaInvalidaException;
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 public record Endereco(String cep, String bairro, String localidade, String estado, String regiao) {
+    public static Endereco buscaEndereco(String busca) throws IOException, InterruptedException {
+        String endereco = "https://viacep.com.br/ws/" + URLEncoder.encode(busca, StandardCharsets.UTF_8) + "/json";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() == 400 || response.statusCode() == 404) {
+            throw new BuscaInvalidaException("CEP inválido ou não encontrado, tente novamente.");
+        }
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson().newBuilder().setPrettyPrinting().create();
+        return gson.fromJson(json, Endereco.class);
+    }
+
+    public static void salvaEnderecos(List<Endereco> enderecos) {
+        try (FileWriter fileWriter = new FileWriter("enderecos.json")) {
+            Gson gson = new Gson().newBuilder().setPrettyPrinting().create();
+            fileWriter.write(gson.toJson(enderecos));
+            System.out.println(enderecos);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/br/com/alura/consultaCep/principais/Main.java
+++ src/br/com/alura/consultaCep/principais/Main.java
@@ -2,23 +2,14 @@ package br.com.alura.consultaCep.principais;
 
 import br.com.alura.consultaCep.excecoes.BuscaInvalidaException;
 import br.com.alura.consultaCep.modelos.Endereco;
-import com.google.gson.Gson;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-    static Gson gson = new Gson().newBuilder().setPrettyPrinting().create();
     public static void main(String[] args) {
         String busca = "";
         List<Endereco> enderecos = new ArrayList<>();
@@ -35,18 +26,7 @@ public class Main {
                     continue;
                 }
 
-                String endereco = "https://viacep.com.br/ws/" + URLEncoder.encode(busca, StandardCharsets.UTF_8) + "/json";
-                HttpClient client = HttpClient.newHttpClient();
-                HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                String json = response.body();
-                System.out.println(json);
-
-                if (response.statusCode() == 400 || response.statusCode() == 404) {
-                    throw new BuscaInvalidaException("CEP inválido ou não encontrado, tente novamente.");
-                }
-
-                Endereco enderecoLocal = gson.fromJson(json, Endereco.class);
+                Endereco enderecoLocal = Endereco.buscaEndereco(busca);
                 System.out.println(enderecoLocal);
                 enderecos.add(enderecoLocal);
             } catch (IOException | InterruptedException e) {
@@ -55,11 +35,6 @@ public class Main {
                 System.out.println(e.getMessage());
             }
         }
-        try (FileWriter fileWriter = new FileWriter("enderecos.json")) {
-            fileWriter.write(gson.toJson(enderecos));
-            System.out.println(enderecos);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        Endereco.salvaEnderecos(enderecos);
     }
 }
